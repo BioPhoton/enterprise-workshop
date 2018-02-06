cc446bed567dfdab986919aff5045046a8021c97
Michael Hladky <michael@hladky.at>
2018-02-06 22:56:17 +0100

    feat(cross-cutting): Guards: CanDeactivateGuard

--- apps/flight-app/src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
 
 @Component({
   selector: 'app-flight-edit',
@@ -10,7 +12,24 @@ export class FlightEditComponent implements OnInit {
   showDetails: string;
   showWarning = false;
 
+  sender: Observer<boolean>;
+  showWarning = false;
+
   constructor(private route: ActivatedRoute) {
+  }
+
+  decide(decision: boolean): void {
+    this.showWarning = false;
+    this.sender.next(decision);
+    this.sender.complete();
+  }
+
+  canDeactivate(): Observable<boolean> {
+    return Observable.create((sender: Observer<boolean>) => {
+      this.sender = sender;
+      this.showWarning = true;
+    });
+
   }
 
   ngOnInit() {
