498a0c8c08fbcdae3fd436030e8e3c8c7076b704
Michael Hladky <michael@hladky.at>
2018-02-07 22:35:30 +0100

    feat(performance): Lazy loading: Hotels and FlightBooking

---
+++ libs/hotels/src/hotels.module.spec.ts
@@ -0,0 +1,7 @@
+import { HotelsModule } from './hotels.module';
+
+describe('HotelsModule', () => {
+  it('should work', () => {
+    expect(new HotelsModule()).toBeDefined();
+  });
+});
---
+++ libs/hotels/src/hotels/hotels.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hotels',
+  templateUrl: './hotels.component.html',
+  styleUrls: ['./hotels.component.css']
+})
+export class HotelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
