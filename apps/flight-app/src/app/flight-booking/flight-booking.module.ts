c3dac9424499ebff0b5709234494afc12c332cb3
Michael Hladky <michael@hladky.at>
2018-02-11 23:12:37 +0100

    feat(Reactive): Forms and bindings

--- apps/flight-app/src/app/flight-booking/flight-booking.module.ts
+++ apps/flight-app/src/app/flight-booking/flight-booking.module.ts
@@ -1,6 +1,6 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {SharedModule} from '../shared/shared.module';
 import {FlightBookingComponent} from './flight-booking.component';
@@ -14,6 +14,7 @@ import {PassengerSearchComponent} from './passenger-search/passenger-search.comp
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule.forChild(),
     RouterModule.forChild(FLIGHT_BOOKING_ROUTES)
   ],
