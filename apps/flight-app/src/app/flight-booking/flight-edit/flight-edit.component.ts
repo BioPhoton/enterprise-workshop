f58660fae7f24d2fe30a75a8b578b6d7bc3699ad
Michael Hladky <michael@hladky.at>
2018-02-11 23:12:37 +0100

    feat(Reactive): Forms and bindings

--- apps/flight-app/src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -1,23 +1,41 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {pluck, switchMap} from 'rxjs/operators';
+import {Observable} from 'rxjs/Observable';
+import {FlightService} from '@flight-workspace/flight-api';
 
 @Component({
   selector: 'app-flight-edit',
   templateUrl: './flight-edit.component.html'
 })
 export class FlightEditComponent implements OnInit {
-  id: string;
-  showDetails: string;
+  id$: Observable<string>;
+  showDetails$: Observable<string>;
+
   showWarning = false;
 
-  constructor(private route: ActivatedRoute) {
+  editForm: FormGroup;
+
+  constructor(private route: ActivatedRoute, private fb: FormBuilder, private flightService: FlightService) {
+    this.editForm = this.fb.group({
+      'id': [],
+      'from': [],
+      'to': [],
+      'date': []
+    })
+
   }
 
   ngOnInit() {
-    this.route.params.subscribe(p => {
-      this.id = p['id'];
-      this.showDetails = p['showDetails'];
-    });
+    this.id$  = this.route.params.pipe(pluck('id'));
+    this.showDetails$  = this.route.params.pipe(pluck('showDetails'));
+
+    this.id$
+      .pipe(
+        switchMap(id => this.flightService.findById(id))
+      )
+      .subscribe(flight => this.editForm.patchValue(flight));
   }
 
 }
