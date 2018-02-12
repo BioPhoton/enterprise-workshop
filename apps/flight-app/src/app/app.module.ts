b2c77ba05595d77991954ebc94970e4b7b02e48b
Michael Hladky <michael@hladky.at>
2018-02-12 01:01:39 +0100

    feat(State): implement root store

--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
-import {FlightApiModule} from '@flight-workspace/flight-api';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FlightApiModule } from '@flight-workspace/flight-api';
 
-import {AppComponent} from './app.component';
-import {APP_EXTRA_OPTIONS, APP_ROUTES} from './app.routes';
-import {BasketComponent} from './basket/basket.component';
-import {FlightBookingModule} from './flight-booking/flight-booking.module';
-import {HomeComponent} from './home/home.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {SharedModule} from './shared/shared.module';
-import {SidebarComponent} from './sidebar/sidebar.component';
+import { AppComponent } from './app.component';
+import { APP_EXTRA_OPTIONS, APP_ROUTES } from './app.routes';
+import { BasketComponent } from './basket/basket.component';
+import { FlightBookingModule } from './flight-booking/flight-booking.module';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SharedModule } from './shared/shared.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { appReducer } from './+state/app.reducer';
+import { appInitialState } from './+state/app.init';
+import { AppEffects } from './+state/app.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   imports: [
@@ -21,17 +29,14 @@ import {SidebarComponent} from './sidebar/sidebar.component';
     FlightBookingModule,
     FlightApiModule.forRoot(),
     SharedModule.forRoot(),
-    RouterModule.forRoot([...APP_ROUTES], {...APP_EXTRA_OPTIONS})
+    RouterModule.forRoot([...APP_ROUTES], { ...APP_EXTRA_OPTIONS }),
+    StoreModule.forRoot({ app: appReducer }, { initialState: { app: appInitialState } }),
+    EffectsModule.forRoot([AppEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule
   ],
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    NavbarComponent,
-    HomeComponent,
-    BasketComponent
-  ],
-  providers: [],
+  declarations: [AppComponent, SidebarComponent, NavbarComponent, HomeComponent, BasketComponent],
+  providers: [AppEffects],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
