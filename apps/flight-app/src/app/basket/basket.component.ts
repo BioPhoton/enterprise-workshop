dc53658ceff7c9f585f9a0285acb2cd2e6b35939
Michael Hladky <michael@hladky.at>
2018-02-14 19:25:12 +0100

    formatting

--- apps/flight-app/src/app/basket/basket.component.ts
+++ apps/flight-app/src/app/basket/basket.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'basket',
@@ -6,9 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./basket.component.css']
 })
 export class BasketComponent {
-
-  constructor() {
-  }
-
-
+  constructor() {}
 }
--- apps/flight-app/src/app/flight-booking/flight-booking.routes.ts
+++ apps/flight-app/src/app/flight-booking/flight-booking.routes.ts
@@ -1,8 +1,8 @@
-import {Routes} from '@angular/router';
-import {FlightBookingComponent} from './flight-booking.component';
-import {FlightEditComponent} from './flight-edit/flight-edit.component';
-import {FlightSearchComponent} from './flight-search/flight-search.component';
-import {PassengerSearchComponent} from './passenger-search/passenger-search.component';
+import { Routes } from '@angular/router';
+import { FlightBookingComponent } from './flight-booking.component';
+import { FlightEditComponent } from './flight-edit/flight-edit.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
 
 export const FLIGHT_BOOKING_ROUTES: Routes = [
   {
@@ -23,5 +23,4 @@ export const FLIGHT_BOOKING_ROUTES: Routes = [
       }
     ]
   }
-
-]
+];
