e80e4fb4e136e6f2fad788b992b240eee9333e72
Michael Hladky <michael@hladky.at>
2018-04-02 19:22:59 +0200

    merge

--- apps/flight-app/src/app/flight-booking/flight-card/flight-card.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-card/flight-card.component.ts
@@ -14,6 +14,7 @@ import {
 import { Flight } from '@flight-workspace/flight-api';
 
 @Component({
+  selector: 'flight-card',
   templateUrl: './flight-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
